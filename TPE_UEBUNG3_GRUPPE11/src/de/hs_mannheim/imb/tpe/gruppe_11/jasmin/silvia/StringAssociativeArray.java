d4fdd57420343673ca68704ddb33621c8eec7880
silvia.yildiz@hotmail.de <s.yildiz@141.19.145.26>
2014-11-24 18:49:05 +0100

    Die erste Klasse die wir implementiert haben

---
+++ TPE_UEBUNG3_GRUPPE11/src/de/hs_mannheim/imb/tpe/gruppe_11/jasmin/silvia/StringAssociativeArray.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package de.hs_mannheim.imb.tpe.gruppe_11.jasmin.silvia;
+
+/**
+ * @author Silvia Yildiz, Jasmin Cano
+ * 
+ */
+
+public class StringAssociativeArray<U> extends AssociativeArrayImpl<String, U> {
+
+}
